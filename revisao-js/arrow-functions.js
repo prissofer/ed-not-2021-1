bd757a4d3e8ed1fcdcf174789c1faf90b338ef25
prissofer <prissofer@gmail.com>
2021-02-19 01:20:03 +0000

    (18/02) Continuação da revisão JS

--- revisao-js/arrow-functions.js
+++ revisao-js/arrow-functions.js
@@ -8,7 +8,7 @@ console.log(quadrado(9))
 
 // Carcterística da função acima:
 // 1) Apenas um parâmetro
-// 2) Apenas uma lonha de códigoo, com return
+// 2) Apenas uma linha de códigoo, com return
 // 3) CANDIDATA PERFEITA A VIRAR ARROW FUNCTION
 
 // Transformando em arrow funtiction
@@ -17,3 +17,47 @@ console.log(quadrado(9))
 // c) Não são necessárias as chaves e nem a palavra  return
 const quadrado2 = n => n * n
 console.log(quadrado2(9))
+
+// Função com mais de um parâmetro
+function potencia(b, e) {   // b = base , e = expoente
+    return b ** e
+}
+console.log(potencia(2, 8))
+
+//Função equivalente no formato de arrow function
+// Quando temos mais de parâmetro, os parênteses em volta dos argumentos voltam a ser obrigatórios
+let potencia2 = (b, e) => b ** e
+console.log(potencia2(2, 8))
+
+// Função sem parâmetro
+function megasena() {
+    // Esta função retorna um número aleatório entre 1 e 60
+    // Math.randow() -> retornaum número aleatório entre 0 (inclusive) e 1 (exclusive)
+    // *60 -> ajusta a faixa de valores para 0 e 59
+    // + 1 -> ajusta a faixa de valores para enter 1 e 60
+    // Math.floor -> arredonda para o menor inteiro (tirar as casas decimais)
+    return Math.floor(Math.random() * 60 + 1)
+}
+console.log(megasena(), megasena(), megasena(), megasena(), megasena(), megasena())
+
+// Quando a função não tem argumentos, na arrow function é necessário deixar uma par de parênteses vazios para marcar seu lugar
+const megasena2 = () => Math.floor(Math.random() * 60 + 1)
+console.log(megasena2(), megasena2(), megasena2(), megasena2(), megasena2(), megasena2())
+
+// Função com mais de uma linha de código no corpo
+function somaVet(vet)   {
+    let soma = 0
+    for (let n of vet) soma += n
+    return soma
+}
+console.log(somaVet([12, 42, -11, 20, 9, 16]))
+
+// Quando a função tem mais de uma linha de código, na arrow function equivalente, retornam as chaves e também a palavra reurn, caso ela retorne valor.
+const somaVet2 = vet => {
+    let soma = 0
+    for (let n of vet) soma += n
+    return soma
+}
+console.log(somaVet2([12, 42, -11, 20, 9, 16]))
+
+// Conclusão: arrow functions foram concebidas para facilitar a escrita de funções de apenas uma linha, embora também possam ser usadas para funções múltiplas linhas.
---
+++ revisao-js/objetos.js
@@ -0,0 +1,65 @@
+// Objetos são estruturas que permitem armazenar diversos valores em uma mesma variável, permitindo acessá-los por meio de um nome
+// Criação de um objeto vazio 
+let vazio1 = {}     // Forma "moderna"
+let vazio2 = new Object() //Forma "tradicional"
+
+// Criando um objeto já com dados
+let pessoa = {
+    // Os dados são armazenados no formato
+    // propriedade: valor ou também como
+    // atributo: valor
+    nome: 'Orkutilson de Oliveira',
+    sexo: 'M',
+    dataNasc: '2008-04-19',
+    ocupacao: 'estudante',
+    // Nome da propriedade pode ter espaçoes ou acentos
+    // Nesse caso, o nome da propriedade prisoca estar entre aspas simples
+    'cidade de origem': 'Franca/SP',
+    gostos: ['jogos', 'doces', 'dormir'] // Vetor em um posição de objeto
+}
+console.log(pessoa)
+console.table(pessoa)
+
+// Exibindo apenas o valor de uma propriedade
+console.log(pessoa.nome)
+console.log(pessoa.ocupacao)
+
+// Quando o nome da propriedade tem espaços ou acentos, não é possível usar a sintaxe de ponto (.). É necessário usat a sintaxe de colchetes.
+console.log(pessoa['cidade de origem'])
+console.log(pessoa.gostos)
+console.log(pessoa.gostos[2])
+
+// A sintaxe dos colchete SEMPRE funciona, mesmo se o nome da propriedade for uma única paralavra sem acento
+console.log(pessoa['nome'])
+console.log(pessoa['gostos'][2])     // Monstrando só o 3º gosto
+
+// Outra consequência da sintaxe dos colchetes é a possibilidade de usar nomes de propeiedades que estão dentro de variáveis
+
+let x = 'sexo'
+console.log(pessoa[x])
+x = 'dataNasc'
+console.log(pessoa[x])
+
+// Criando uma nova propriedade para um objeto já existente
+// Nasta atribuir um valor a um nome de propriedade ainda nao existente
+pessoa.email = 'orkutilson@gamil.com'
+pessoa.celular = '(16) 98765-4321'
+console.table(pessoa)
+
+// Criando um objeto vazio e só depois adicionando propriedaddes
+let carro = {}
+carro.marca = 'Volkswagen'
+carro.modelo = 'Kombi'
+carro.ano = 1971
+carro['combstível'] = 'gasolina'
+carro.cor = 'bege'
+
+console.table(carro)
+
+// Excluindo uma propriedade de um objeto
+delete carro.ano
+console.table(carro)
+
+
+
+
